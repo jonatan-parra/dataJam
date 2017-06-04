8cacb7d190a53a3245810ac47d77890545846aae
s13g3lb4um <s13g3lb4um@gmail.com>
2017-06-04 12:48:41 -0500

    Arreglos

--- js/csv.js
+++ js/csv.js
@@ -1,14 +1,65 @@
-function csv(dir) {
+// function csv(dir) {
+//   $.ajax({
+//     url: './docs/' + dir,
+//     dataType: 'text',
+//     // async: false
+//   }).done(successFunction)
+// }
+//
+// function successFunction(data) {
+//   var allRows = data.split(/\r?\n|\r/);
+//   var info = []
+//   var field = []
+//   for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+//     var rowCells = allRows[singleRow].split(';');
+//     for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+//       info.push(rowCells[rowCell])
+//     }
+//     field.push(info)
+//     info = []
+//   }
+//   console.log("prueba " + field );
+//   devolverCSV(field);
+// }
+// var localCSV;
+// function devolverCSV(data){
+//     localCSV = data;
+// }
+// function traerCSV(url){
+//   csv(url);
+//   var lo = localCSV;
+//   localCSV=null;
+//   return lo;
+// }
+
+function csv(dir, func) {
+  var a = func
   $.ajax({
-    url: './docs/'+dir,
+    url: './docs/' + dir,
     dataType: 'text',
-  }).done(successFunction);
+    // async: false
+  }).done(successFunction)
+  function successFunction(data) {
+    var allRows = data.split(/\r?\n|\r/);
+    var info = []
+    var field = []
+    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+      var rowCells = allRows[singleRow].split(';');
+      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+        info.push(rowCells[rowCell])
+      }
+      field.push(info)
+      info = []
+      func(field)
+    }
+
+  }
 }
 
 function successFunction(data) {
   var allRows = data.split(/\r?\n|\r/);
-  var field = []
   var info = []
+  var field = []
   for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
     var rowCells = allRows[singleRow].split(';');
     for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
@@ -16,6 +67,7 @@ function successFunction(data) {
     }
     field.push(info)
     info = []
+    a(field)
   }
-  return field
+
 }
--- js/motivacion.js
+++ js/motivacion.js
@@ -1,37 +1,84 @@
-var ctx = document.getElementById("histoChart");
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
+Papa.parse('./docs/motivacion.csv', {
+  download: true,
+  complete: function(results) {
+
+    var ctx = document.getElementById("histoChart").getContext("2d");
+
+    dt = []
+    lb = []
+    cl = []
+    for (var i = 0; i < results.data.length; i++) {
+      temp = results.data[i]
+      lb.push(temp[1]);
     }
+    var barChartData = {
+      labels: lb,
+      datasets: [{
+        label: 'Dataset 1',
+        backgroundColor: window.chartColors.red,
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }, {
+        label: 'Dataset 2',
+        backgroundColor: window.chartColors.blue,
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }, {
+        label: 'Dataset 3',
+        backgroundColor: window.chartColors.green,
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }]
+
+    };
+
+    console.log(results.data.length);
+    for (var i = 0; i < results.data.length; i++) {
+      temp = results.data[i]
+      lb.push(temp[1]);
+      console.log(temp[1]);
+    }
+    for (var i = 1; i < results.data.length; i++) {
+      for (var j = 0; j < results.data[i].length; j++) {
+        temp = results.data[i]
+        dt.push(temp[j]);
+      }
+    }
+    console.log(dt);
+    var config = {
+      type: 'bar',
+      data: {
+        datasets: [{
+          data: dt,
+          label: 'Motivacion'
+        }],
+        labels: lb
+      },
+      options: {
+        responsive: true
+      }
+    };
+    window.myBar = new Chart(ctx, config);
   }
 });
