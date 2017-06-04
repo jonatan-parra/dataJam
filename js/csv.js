2e858fb9f69ec629c512705c89e9410e86fc2370
s13g3lb4um <s13g3lb4um@gmail.com>
2017-06-04 09:31:21 -0500

    Generalización de lectura de CSV

--- js/csv.js
+++ js/csv.js
@@ -1,26 +1,28 @@
-function csv(x) {
+function csv(dir) {
   $.ajax({
-    url: x,
+    url: './docs/'+dir,
     dataType: 'text',
   }).done(successFunction);
 }
 
 function successFunction(data) {
   var allRows = data.split(/\r?\n|\r/);
-  var labels = []
   var field = []
   var info = []
   for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
     var rowCells = allRows[singleRow].split(';');
     for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
-      if (singleRow === 0) {
-        labels.push(rowCells[rowCell])
-      } else {
-        info.push(rowCells[rowCell])
-      }
+      info.push(rowCells[rowCell])
+      // if (singleRow === 0) {
+      //   labels.push(rowCells[rowCell])
+      //   field.push(labels)
+      // } else {
+      //   info.push(rowCells[rowCell])
+      // }
     }
     field.push(info)
-    console.log(info);
     info = []
   }
+  console.log(field);
+  return field
 }
