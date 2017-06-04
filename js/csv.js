022a806cf6ce02c8beed03711fabec048aef461f
s13g3lb4um <s13g3lb4um@gmail.com>
2017-06-04 09:34:56 -0500

    Arreglos menores en csv.js

--- js/csv.js
+++ js/csv.js
@@ -13,12 +13,6 @@ function successFunction(data) {
     var rowCells = allRows[singleRow].split(';');
     for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
       info.push(rowCells[rowCell])
-      // if (singleRow === 0) {
-      //   labels.push(rowCells[rowCell])
-      //   field.push(labels)
-      // } else {
-      //   info.push(rowCells[rowCell])
-      // }
     }
     field.push(info)
     info = []
